b512fc55c707488024ec2dabbe3ed83004a2d29e
DanielleSmith0 <Danielahsmith@icloud.com>
2023-03-23 15:30:37 -0600

    initial commit/express running

--- server.js
+++ server.js
@@ -1,19 +1,9 @@
 const express = require("express");
 
-const app = express;
+const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html')
-});
-
-app.get('/css', (req, res) => {
-    res.sendFile(__dirname + '/public/styles.css')
-});
-
-app.get('/js', (req, res) => {
-    res.sendFile(__dirname + '/public/main.js')
-});
+app.use(express.static(`${__dirname}/public`))
 
 app.listen(4321, () => {
     console.log("up on 4321")
