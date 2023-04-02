3cf4eddc4b7738fe7fe4c72239b60a93215e2d31
DanielleSmith0 <Danielahsmith@icloud.com>
2023-04-01 21:54:41 -0600

    deployment completed

--- server.js
+++ server.js
@@ -3,7 +3,16 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/../public`));
+
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'c31a6e6f6e90426297ddcfc64e472520',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
+
+rollbar.log("howdy")
 
 app.listen(4321, () => {
     console.log("up on 4321")
